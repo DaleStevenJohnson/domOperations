d4c44573aaef380fd3e0543595edb1eeef7922fc
Dale Johnson <40277984+DaleStevenJohnson@users.noreply.github.com>
2018-10-18 16:20:45 +0100

    Uploaded current version of domOperations to Git

---
+++ domOperations.js
@@ -0,0 +1,94 @@
+let dom = {
+	getElement: function(e) {
+		if(typeof e == "string") {
+			e = document.getElementById(e);
+		}
+		return e;
+	},
+
+	createElementWithClass: function(e, c) {
+		let a = document.createElement(e);
+		if(c !== undefined) {
+			a.classList.add(c);
+		}
+		return a;
+	},
+	hideElement: function(e) {
+		e = this.getElement(e);
+		e.style.visibility = "hidden";
+		e.style.display = "none";
+	},
+	hideElements: function(arr) {
+		for(let a of arr) {
+			this.hideElement(a);
+		}
+	},
+	hideAShowB: function(a, b) {
+		a = this.getElement(a);
+		b = this.getElement(b);
+		this.hideElement(a);
+		this.showElement(b);
+	},
+	showElement: function(e) {
+		e = this.getElement(e);
+		e.style.visibility = "visible";
+		e.style.display = "block";
+	},
+	hideChildren: function(e) {
+		e = this.getElement(e);
+		for (let i = e.children.length-1; i >= 0; i--) {
+			this.hideElement(e.children[i]);
+		}
+	},
+	killChildren: function(e) {
+		e = this.getElement(e);
+		for (let i = e.children.length-1; i >= 0; i--) {
+			e.removeChild(e.children[i]);
+		}
+	},
+	killElement: function(e) {
+		e.parentNode.removeChild(e);
+	},
+	iframeLoaded: function(iframe) {
+		let parent = iframe.parentElement
+		this.showElement(parent);
+		let a = iframe.contentWindow.document.body.scrollHeight;
+		let b = 50;
+		iframe.style.height = (a+b) + "px";
+        this.hideElement(parent);
+        iframe.contentWindow.document.body.style.overflowY = "hidden";
+  	},
+  	getImageSize: function (img, callback) {
+  		console.log(img);
+	    let wait = setInterval(
+	    	function() {
+		        let w = img.naturalWidth;
+		        let h = img.naturalHeight;
+		        if (w && h) {
+		            clearInterval(wait);
+		            callback.apply(this, [w, h, img]);
+		            console.log(img);
+	        	}
+	    	}, 
+	    30);
+	},
+	inputIsClean: function(string) {
+		let a = new RegExp(/[<>*&|=\]\/]/);
+		 return !a.test(string);
+	},
+
+	downloadFile: function(location, name) {
+		let a = document.createElement("A");
+		a.href=location;
+		a.download=name;
+		a.click();
+	},
+
+	changeElementBackgroundImage: function(e, source) {
+		e = this.getElement(e);
+		e.style.backgroundImage = `url(${source})`;
+	}
+
+
+}
+
